1f57662937d3b10d77a20e31c05e217adc549a35
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2021-02-15 16:56:36 +0800

    add item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,28 +29,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(title:Text("Welcome to My app")),
-      body: Column(
-        children: [
-          Text("Hello World", style: TextStyle(fontSize: 30, color: Colors.red),),
-          SizedBox(height: 10),
-          Text("Welcome to my app!"),
-          SizedBox(height: 10),
-          Image.network("https://cdn.britannica.com/s:800x450,c:crop/79/189379-138-C16A98A4/video-Kuala-Lumpur-Malaysia.jpg"),
-          Text("This is my first app"),
-          SizedBox(height: 10),
-          Text("Jom cuba lagi!"),
-          Row(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
             children: [
-              Text("First col of my row"),
-              SizedBox(width: 10,),
-              Text("Second col of my row"),
-              SizedBox(width: 10,),
-              Text("Third col of my row")
+              Text("Hello World", style: TextStyle(fontSize: 30, color: Colors.red),),
+              SizedBox(height: 10),
+              Text("Welcome to my app!"),
+              SizedBox(height: 10),
+              Image.network("https://cdn.britannica.com/s:800x450,c:crop/79/189379-138-C16A98A4/video-Kuala-Lumpur-Malaysia.jpg"),
+              Text("This is my first app"),
+              SizedBox(height: 10),
+              Text("Jom cuba lagi!"),
+              Row(
+                children: [
+                  Text("one"),
+                  SizedBox(width: 10,),
+                  Text("two"),
+                  SizedBox(width: 10,),
+                  Text("three")
+                ],
+              ),
+              TextField(decoration: InputDecoration(labelText: 'What is your name'), controller: nameController,),
+              FlatButton(color: Colors.yellow, onPressed: (){ print("Hello ${nameController.text}!"); }, child: Text("Press me!")),
+              RaisedButton(onPressed: (){
+                Toast.show("Hello Toast", context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
+              },
+                  child:Text("Show Toast"), color:Colors.blue),
+              RaisedButton(onPressed: (){
+                final snackBar = SnackBar(content: Text("yay! A Snackbar"));
+                Scaffold.of(context).showSnackBar(snackBar);
+                }, child:Text("Show Snackbar"))
             ],
           ),
-          TextField(decoration: InputDecoration(labelText: 'What is your name'), controller: nameController,),
-          FlatButton(color: Colors.yellow, onPressed: (){ print("Hello ${nameController.text}!"); }, child: Text("Press me!"))
-        ],
+        ),
       )
     );
   }
